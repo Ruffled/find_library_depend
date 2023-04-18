c7bdc71981e7ba9590d87074600afb0895d1bbf7
Ruffled <middlefraz@yahoo.com>
2023-04-18 13:53:26 -0400

    Update check-libs.sh

--- check-libs.sh
+++ check-libs.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 echo "generate dynamic list"
-find . -executable | xargs file | grep EABI.*SYSV.*dynamic | sed 's/:.*//' > /tmp/dynamic_list 2>/dev/null
+find . -executable 2>/dev/null | xargs file | grep EABI.*SYSV.*dynamic | sed 's/:.*//' > /tmp/dynamic_list
 echo "generate depend list"
-cat /tmp/dynamic_list | xargs readelf -d | grep NEEDED | sort -u | sed -e 's/.*\[//' -e 's/\].*//' > /tmp/depended_libs 2>/dev/null
+cat /tmp/dynamic_list | xargs readelf -d | grep NEEDED | sort -u | sed -e 's/.*\[//' -e 's/\].*//' > /tmp/depended_libs
 echo "look for dependent files"
-cat /tmp/depended_libs | while read LL; do LLL=$(find . -name $LL); if [ -z "$LLL" ]; then echo $LL appears to be missing; fi; done 2>/dev/null
+cat /tmp/depended_libs | while read LL; do LLL=$(find . -name $LL 2>/dev/null); if [ -z "$LLL" ]; then echo $LL appears to be missing; fi; done
 echo "done"
